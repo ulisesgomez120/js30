f406dfa38bf9a2a27888663082c6ac423e3e7848
ulisesgomez120 <ulisesgomez120@gmail.com>
2019-08-16 07:17:47 -0700

    added some selectors for the video player

---
+++ htmlPlayer/scripts.js
@@ -0,0 +1,8 @@
+const player = document.querySelector(".player");
+const video = document.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
